9f0f63590b804a2dafcffd4d000d61187c78b93d
thebestbadfriend <51896268+thebestbadfriend@users.noreply.github.com>
2023-04-08 20:38:58 -0500

    Add files via upload

---
+++ Extract-ChildArchives.ps1
@@ -0,0 +1,20 @@
+﻿param(
+    [switch]$Cleanup
+);
+
+foreach ($item in $(Get-ChildItem))
+{
+    $item = $item.ToString()
+    $itemBaseName = $item.Substring(0,$item.LastIndexOf("."))
+    "============================="
+    "extracting $item into folder: $itemBaseName"
+    Expand-Archive -Path $item -DestinationPath $itemBaseName
+    if ($Cleanup)
+    {
+        "done extracting $item"
+        "deleting $item"
+        Remove-Item -Path $item
+    }
+    "done working on $item"
+    "============================="
+}
